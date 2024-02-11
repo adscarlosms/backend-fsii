4ac266c429fccf2230ad4c802fce728f20e0dc60
adscarlosms <menezes.carlosms@gmail.com>
2024-02-11 10:06:30 -0300

    relacionando entidades - produto x categoria

--- Modelo/produto.js
+++ Modelo/produto.js
@@ -7,10 +7,11 @@ export default class Produto{
     #precoVenda;
     #dataValidade;
     #qtdEstoque;
+    #categoria;
 
 
     constructor(codigo=0,descricao="", precoCusto=0, 
-                precoVenda=0,dataValidade='', qtdEstoque=0
+                precoVenda=0,dataValidade='', qtdEstoque=0, categoria={}
                 ){
         this.#codigo=codigo;
         this.#descricao=descricao;
@@ -18,6 +19,7 @@ export default class Produto{
         this.#precoVenda=precoVenda;
         this.#dataValidade=dataValidade;
         this.#qtdEstoque=qtdEstoque;
+        this.#categoria=categoria;
     }
 
     get codigo(){
@@ -60,11 +62,19 @@ export default class Produto{
     }
 
     get qtdEstoque(){
-        return this.#dataValidade;
+        return this.#qtdEstoque;
     }
 
     set qtdEstoque(novaQtd){
         this.#qtdEstoque = novaQtd;
+    }
+
+    get categoria(){
+        return this.#categoria;
+    }
+
+    set categoria(novaCat){
+        this.#categoria = novaCat;
     }
 
 
@@ -76,6 +86,7 @@ export default class Produto{
             precoVenda:this.#precoVenda,
             dataValidade:this.#dataValidade,
             qtdEstoque:this.#qtdEstoque,
+            categoria:this.#categoria,
         }
     }
 
